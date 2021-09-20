5fce9a61b9e994abb1b16fcd7ed85d4f8e9a339a
DELL <bathla.raghav5@gmail.com>
2021-09-20 09:56:31 +0530

    Hello

---
+++ src/Final/FinalClass.java
@@ -0,0 +1,9 @@
+package Final;
+
+public class FinalClass {
+    final int valueOfPie = 22/7;
+    final int areaOfCircle(int radius){
+return valueOfPie*radius*radius;
+    }
+
+}
---
+++ src/Final/FinalMain.java
@@ -0,0 +1,8 @@
+package Final;
+
+public class FinalMain {
+    public static void main(String[] args) {
+FinalClass finalClass = new FinalClass();
+System.out.println(finalClass.areaOfCircle(40));
+    }
+}
